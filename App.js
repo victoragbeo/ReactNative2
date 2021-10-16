7c33f743529903695f265a2766cc8de23ec5a6c2
Victor Agbeo <75548415+saintvic@users.noreply.github.com>
2021-10-16 12:24:56 -0500

    Week 3- Workshop

--- App.js
+++ App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
     return (
         // wrapping Provider n passing store as a prop gives
         // main component n children access to redux store
         <Provider store={store}>
-            <Main />
+            <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
         </Provider>
     );
 }
